7ecf161b1bf9486b47d56773021d5a63a338a1c9
firko-learnin <jackfirkin@gmail.com>
2022-03-18 10:36:08 +0000

    Dashboard links styling

--- Components/LinksArea/index.tsx
+++ Components/LinksArea/index.tsx
@@ -3,38 +3,59 @@ import menu from "../../public/menu.jpg";
 import machine from "../../public/machines.jpg";
 import roaster from "../../public/roasters.jpg";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function LinksArea() {
   return (
     <div className="links">
-      <div>
-        <Link href="./grinders">
-          <a>
-            <h2>Grinders</h2>
-            <img src={grinder} alt="coffee grinder" />
-          </a>
-        </Link>
-        <Link href="./newRecipe">
-          <a>
-            <h2>New Recipe</h2>
-            <img src={menu} alt="coffee menu" />
-          </a>
-        </Link>
-      </div>
-      <div>
-        <Link href="./machines">
-          <a>
-            <h2>Machines</h2>
-            <img src={machine} alt="espresso machine" />
-          </a>
-        </Link>
-        <Link href="./roasters">
-          <a>
-            <h2>Roasters</h2>
-            <img src={roaster} alt="coffee roasting machine" />
-          </a>
-        </Link>
-      </div>
+      <Link href="./grinders">
+        <a style={{ gridArea: "link1" }}>
+          <h2>Grinders</h2>
+          <Image
+            className="links-img"
+            src={grinder}
+            alt="coffee grinder"
+            width={300}
+            height={300}
+          ></Image>
+        </a>
+      </Link>
+      <Link href="./newRecipe">
+        <a style={{ gridArea: "link2" }}>
+          <h2>New Recipe</h2>
+          <Image
+            src={menu}
+            alt="coffee menu"
+            className="links-img"
+            width={300}
+            height={300}
+          />
+        </a>
+      </Link>
+      <Link href="./machines">
+        <a style={{ gridArea: "link3" }}>
+          <h2>Machines</h2>
+          <Image
+            src={machine}
+            alt="espresso machine"
+            className="links-img"
+            width={300}
+            height={300}
+          />
+        </a>
+      </Link>
+      <Link href="./roasters">
+        <a style={{ gridArea: "link4" }}>
+          <h2>Roasters</h2>
+          <Image
+            src={roaster}
+            alt="coffee roasting machine"
+            className="links-img"
+            width={300}
+            height={300}
+          />
+        </a>
+      </Link>
     </div>
   );
 }
